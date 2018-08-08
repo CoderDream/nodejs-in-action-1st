37f37819108f24f0d1892325cec875a6500b1521
CoderDream <coderdream@gmail.com>
2018-08-08 16:19:54 +0800

    chapter030201

---
+++ ch03/ch030202_socket/echo_server_once.js
@@ -0,0 +1,7 @@
+let net = require('net');
+let server = net.createServer(function(socket) {
+    socket.once ('data', function(data) {
+        socket.write(data);
+    });
+});
+server.listen(8888);
