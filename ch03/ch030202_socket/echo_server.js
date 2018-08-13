f58d054ca3b213f6e0065fe565cb1f41f52541df
CoderDream <coderdream@gmail.com>
2018-08-13 11:02:52 +0800

    add chapter030202 comments

--- ch03/ch030202_socket/echo_server.js
+++ ch03/ch030202_socket/echo_server.js
@@ -1,7 +1,12 @@
 let net = require('net');
+
 let server = net.createServer(function(socket) {
+    // 当读取到新数据时处理的data事件
     socket.on('data', function(data) {
+        // 数据被写回到客户端
         socket.write(data);
+        //
+        console.log('data: ' + data);
     });
 });
 server.listen(8888);
