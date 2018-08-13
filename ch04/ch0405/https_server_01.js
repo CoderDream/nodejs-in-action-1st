d932924f7a1501ee3d7cf072690fa0cefe09f52b
CoderDream <coderdream@gmail.com>
2018-08-13 17:51:24 +0800

    add chapter0405 comments

--- ch04/ch0405/https_server_01.js
+++ ch04/ch0405/https_server_01.js
@@ -1,10 +1,15 @@
 let https = require('https');
 let fs = require('fs');
+
+// 作为配置项的SSL密钥和证书
 let options = {
     key: fs.readFileSync('./key.pem'),
     cert: fs.readFileSync('./key-cert.pem')
 };
+
+// 第一个传入的就是配置项对象
 https.createServer(options, function (req, res) {
+    // https和http模块的API几乎一样
     res.writeHead(200);
     res.end("hello world\n");
 }).listen(3000);
