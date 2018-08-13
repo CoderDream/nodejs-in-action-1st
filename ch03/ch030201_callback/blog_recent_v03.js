72d707bba7868f105a193d344e877725e712af36
CoderDream <coderdream@gmail.com>
2018-08-13 10:59:58 +0800

    add chapter030201 comments

--- ch03/ch030201_callback/blog_recent_v03.js
+++ ch03/ch030201_callback/blog_recent_v03.js
@@ -11,14 +11,14 @@ function getTitles(res) {
 
         // 在这里不再创建一个else分支，而是直接return，因为如果出错的话，也没有必要继续执行这个函数了
         getTemplate(JSON.parse(data.toString()), res)
-    })
+    });
 }
 
 function getTemplate(titles, res) {
     fs.readFile('./template.html', function (err, data) {
         if (err) return hadError(err, res);
         formatHtml(titles, data.toString(), res)
-    })
+    });
 }
 
 function formatHtml(titles, tmpl, res) {
